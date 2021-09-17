29425a16746e714d99af0208691dccf2688b10c5
stvvvvvv <stv5458@gmail.com>
2021-09-17 18:00:03 +0300

    New

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="nav"><SignIn class="sign-in"/></nav>
+  <div class="section section-1"></div>
+  <div class="section section-2"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignIn from './components/SignIn.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignIn
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+$font-color: #fdfdfd;
+*{
+  margin: 0;
+  font-family: 'Inter', sans-serif;
+  color: $font-color;
+}
+.nav{
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 50px;
+  background-color: #e9c46a;
+}
+.sign-in{
+  margin-right: 10px;
+  cursor: pointer;
+  transition: all ease-in-out 0.3s;
+  &:hover{
+    color:  darken($font-color,70%);
+    transition: all ease-in-out 0.3s;
+  }
+}
+.section{
+  width: 100%;
+  height: 100vh;
+  &-1{
+    background-color: #264653;
+  }
+  &-2{
+    background-color: #2a9d8f;
+  }
 }
 </style>
---
+++ src/components/SignIn.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>SignIn</h1>
+</template>
+
+<script>
+export default {
+  name: 'SignIn'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
